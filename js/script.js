3c10fbefd66317895c52cccad5c4423332dc598f
axelstromeckerlid2c <150041066+axelstromeckerlid2c@users.noreply.github.com>
2024-05-20 12:50:20 +0200

    Javascript samt mobilversion för delar av hemsidan.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// hamburgermeny
+const burger = document.getElementById('burger')
+const linkList = document.getElementById('link_list')
+
+function toggleMenu() {
+    linkList.classList.toggle('links_visible')
+    burger.classList.toggle('burger_in_x_form')
+}
+
+burger.addEventListener('click', toggleMenu)
